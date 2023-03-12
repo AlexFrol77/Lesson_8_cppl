5b11c2382b0af524f8a47ada118b6d1c782a7c42
Alex Frol <drbo19777@gmail.com>
2023-03-12 23:25:17 +0300

    Init commit. Add Task_1, 2.

---
+++ Task_1/Task_1/Task_1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+template <class T>
+void print_vec(const std::vector<T>& vec) {
+	for (const auto& st : vec) {
+		std::cout << st << " ";
+	}
+	std::cout << std::endl;
+}
+
+int main(int argc, char** argv) {
+
+	setlocale(LC_ALL, "rus");
+
+	std::vector<int> vec{ 1, 1, 2, 5, 9, 6, 1, 7, 9, 2, 7, 4 };
+
+	std::cout << "[IN]: ";
+	print_vec(vec);
+
+	std::sort(vec.begin(), vec.end());
+	auto it = std::unique(vec.begin(), vec.end());
+	vec.erase(it, vec.end());
+
+	std::cout << "[OUT]: ";
+	print_vec(vec);
+
+	return 0;
+}
---
+++ Task_2/Task_2/Task_2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <list>
+#include <forward_list>
+#include <vector>
+#include <string>
+
+template <class T>
+void print_container(T begin, T end) {
+	while (begin != end) {
+		std::cout << *(begin++) << " ";
+		//std::advance(begin, 1);         // экспериментировал)))
+	}
+	std::cout << std::endl;
+}
+
+int main(int argc, char** argv) {
+
+	setlocale(LC_ALL, "rus");
+
+	std::set<std::string> test_set = { "one", "two", "three", "four"};
+	print_container(test_set.cbegin(), test_set.cend());
+
+	std::unordered_set<std::string> test_uno_set = { "one", "two", "three", "four" };
+	print_container(test_uno_set.cbegin(), test_uno_set.cend());
+
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	print_container(test_list.cbegin(), test_list.cend());
+
+	std::forward_list<std::string> test_forward_list = { "one", "two", "three", "four" };
+	print_container(test_forward_list.cbegin(), test_forward_list.cend());
+
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+	print_container(test_vector.cbegin(), test_vector.cend());
+
+	return 0;
+}
